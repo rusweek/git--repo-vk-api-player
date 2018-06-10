a7fa649081763a0da79f41eb4fe4f074803acd52
rusweek <rusweek@gmail.com>
2018-06-10 18:21:02 +0400

    надо в  localhost vk прописать локальный ip

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 window.addEventListener('load', function(e){
-    VK.init({
-        apiId: 6601672
-    });
-
-    VK.Auth.login(function(response){
+   VK.Auth.login(function(response){
         if (response.session) {
             console.log('все нештяк');
         } else {
             console.error('все в говне');
             
         }
-    }, 8);
-
+    }, 8)
+    
 
 });
