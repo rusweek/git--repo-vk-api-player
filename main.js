5abf50498ee7f6de97dd1751980bb53a5fbf5259
rusweek <rusweek@gmail.com>
2018-06-10 20:15:02 +0400

    все сделал как надо. Ошибка была в невнимательности. и всего - то

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 window.addEventListener('load', function(e){
-   VK.Auth.login(function(response){
-        if (response.session) {
-            console.log('все нештяк');
-        } else {
-            console.error('все в говне');
-            
-        }
-    }, 8)
+    new Promise(function(resolve,reject){
+        VK.Auth.login(function(response){
+            if (response.session) {
+                resolve(response);
+                console.log('все нештяк');
+            } else {
+                console.error('все в говне');
+                reject(new Error('Error'));
+                
+            }
+        }, 8);
+    }).then(function(){return new Promise (function(resolve, reject){
+        
+        VK.api('users.get', { v:"5.73", 'name_case': 'gen'}, function(response){
+            console.log(response);
+            if (resolve.error){
+                reject(new Error(response.error.error_msg));
+            } else{
+                console.log(response);
+                console.log(response.response[0].first_name);
+                console.log(response.response[0].last_name);
+                let headerInfo = document.querySelector('.headerInfo');
+                console.log(headerInfo);
+                headerInfo.textContent = `Музыка на странице ${response.response[0].first_name} ${response.response[0].last_name}`;
+                resolve();
+            }
+        })
+    });})
+  
+
+    
     
 
 });
